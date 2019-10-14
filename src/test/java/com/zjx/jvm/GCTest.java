5427c5d1771a02feaa0c11e2c8c2f648828b403b
zhengjiaxing <zhengjiaxing@spek.hk>
2019-10-14 17:52:21 +0800

    update

--- src/test/java/com/zjx/jvm/GCTest.java
+++ src/test/java/com/zjx/jvm/GCTest.java
@@ -17,11 +17,24 @@ public class GCTest {
      * -Xmn10M 新生代内存
      * -XX:+PrintGCDetails 收集器日志参数
      * -XX:SurvivorRatio=8 新生代中Eden区与一个Survivor区空间比例为8:1, 即新生代10M中，Eden区占了8M
+     *
      * <p>
      * 最大堆内存与最小堆内存设置成一样限制了Java堆大小为20M，不可扩展
      * Eden区与Survivor区（2个）占比80%和20%
      * 两个Survivor区最大的好处就是解决了碎片化
      * 永远有一个survivor space是空的，另一个非空的survivor space无碎片
+     *
+     * GC日志分析：
+     * [GC (Allocation Failure) [PSYoungGen: 7827K->1016K(9216K)] 7827K->5311K(19456K), 0.0376950 secs] [Times: user=0.06 sys=0.00, real=0.05 secs]
+     *
+     * 1.GC： Minor GC 还是 Full GC，这里的 GC 表明本次发生的是 Minor GC.
+     * 2.Allocation Failure：引起垃圾回收的原因. 本次GC是因为年轻代中没有任何合适的区域能够存放需要分配的数据结构而触发的.
+     * 3.PSYoungGen：使用的垃圾收集器的名字
+     * 4.7827K->1016K：在本次垃圾收集之前和之后的年轻代内存使用情况.
+     * 5.(9216K)：年轻代的总的大小.
+     * 6.7827K->5311K：在本次垃圾收集之前和之后整个堆内存的使用情况
+     * 7.(19456K)：总的可用的堆内存
+     *
      */
     public static void testAllocation() {
         byte[] allocation1, allocation2, allocation3, allocation4;
--- src/test/java/com/zjx/thread/demo/Mutex.java
+++ src/test/java/com/zjx/thread/demo/Mutex.java
@@ -1,5 +1,7 @@
 package com.zjx.thread.demo;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 import java.util.concurrent.locks.Condition;
@@ -19,7 +21,7 @@ class Mutex implements Lock {
             return getState() == 1;
         }
 
-        // 当状态为0的时候获取锁
+        //当状态为0的时候获取锁，CAS操作成功，则state状态为1，
         public boolean tryAcquire(int acquires) {
             if (compareAndSetState(0, 1)) {
                 setExclusiveOwnerThread(Thread.currentThread());
@@ -46,6 +48,7 @@ class Mutex implements Lock {
     // 仅需要将操作代理到Sync上即可
     private final Sync sync = new Sync();
 
+    //加锁操作，代理到acquire（模板方法）上就行，acquire会调用我们重写的tryAcquire方法
     public void lock() {
         sync.acquire(1);
     }
@@ -54,6 +57,7 @@ class Mutex implements Lock {
         return sync.tryAcquire(1);
     }
 
+    //释放锁，代理到release（模板方法）上就行，release会调用我们重写的tryRelease方法。
     public void unlock() {
         sync.release(1);
     }
@@ -77,4 +81,72 @@ class Mutex implements Lock {
     public boolean tryLock(long timeout, TimeUnit unit) throws InterruptedException {
         return sync.tryAcquireNanos(1, unit.toNanos(timeout));
     }
+
+
+    /**
+     * 测试
+     */
+    private static CyclicBarrier barrier = new CyclicBarrier(31);
+    private static int a = 0;
+    private static  Mutex mutex = new Mutex();
+
+    public static void main (String[] args) throws BrokenBarrierException, InterruptedException {
+        //说明:我们启用30个线程，每个线程对i自加10000次，同步正常的话，最终结果应为300000；
+        //未加锁前
+        for(int i=0;i<30;i++){
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for(int i=0;i<10000;i++){
+                        increment1();//没有同步措施的a++；
+                    }
+                    try {
+                        barrier.await();//等30个线程累加完毕
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            t.start();
+        }
+        barrier.await();
+        System.out.println("加锁前，a="+a);
+
+        //加锁后
+        barrier.reset();//重置CyclicBarrier
+        a=0;
+        for(int i=0;i<30;i++){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for(int i=0;i<10000;i++){
+                        increment2();//a++采用Mutex进行同步处理
+                    }
+                    try {
+                        barrier.await();//等30个线程累加完毕
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+        barrier.await();
+        System.out.println("加锁后，a="+a);
+    }
+    /**
+     * 没有同步措施的a++
+     * @return
+     */
+    public static void increment1(){
+        a++;
+    }
+    /**
+     * 使用自定义的Mutex进行同步处理的a++
+     */
+    public static void increment2(){
+        mutex.lock();
+        a++;
+        mutex.unlock();
+    }
+
 }
